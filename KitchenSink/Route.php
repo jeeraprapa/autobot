886f984748b8e5f309113a8b56a8289b40b8b6ff
jeeraprapa <jeeraprapa.h@gmail.com>
2019-08-08 11:30:45 +0700

    Update Route.php

--- KitchenSink/Route.php
+++ KitchenSink/Route.php
@@ -62,35 +62,9 @@ class Route
             $bot = $this->bot;
             /** @var \Monolog\Logger $logger */
             $logger = $this->logger;
-            $logger->info('Test message event has come');
-            error_log("Failed to connect to database!", 0);
-            $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
-            if (empty($signature)) {
-                return $res->withStatus(400, 'Bad Request');
-            }
-            // Check request with signature and parse request
-            try {
-                error_log($req->getBody());
-                $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
-            } catch (InvalidSignatureException $e) {
-                return $res->withStatus(400, 'Invalid signature');
-            } catch (InvalidEventRequestException $e) {
-                return $res->withStatus(400, "Invalid event request");
-            }
-            foreach ($events as $event) {
-                if (!($event instanceof MessageEvent)) {
-                    $logger->info('Non message event has come');
-                    continue;
-                }
-                if (!($event instanceof TextMessage)) {
-                    $logger->info('Non text message has come');
-                    continue;
-                }
-                $replyText = $event->getText();
+            $replyText = $event->getText();
                 $logger->info('Reply text: ' . $replyText);
                 $resp = $bot->replyText($event->getReplyToken(), $replyText);
-                $logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-            }
             $res->write('OK');
             return $res;
         });
